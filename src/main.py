682988ab3c9de8e117ffe390f6a51341d05383ed
ee3210722 <harshitgupta.iitd25@gmail.com>
2024-04-25 05:16:14 +0530

    configured cloudinary in opencv

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI,File,UploadFile
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 import argparse
 import uvicorn
 import cv2
@@ -11,6 +12,8 @@ from PIL import Image
 import json
 from json import JSONEncoder
 import base64
+import os
+import cloudinary.uploader
 
 class NumpyArrayEncoder(JSONEncoder):
     def default(self, obj):
@@ -20,11 +23,21 @@ class NumpyArrayEncoder(JSONEncoder):
 
 app=FastAPI()
 
-# def check_bounding_box():
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["POST","PUT","DELETE","GET","OPTIONS"],
+)
 
 
+# Configure cloudinary with your credentials
+cloudinary.config(
+  cloud_name = "harshitcloud",
+  api_key =  "421415441673459",
+  api_secret = '8iqlOAjBj3XKrgNQl0W9Oc6o_NU'
+)
 
-@app.get("/make_bounding_box")
+@app.put("/ml/make_bounding_box")
 async def make_bounding_box(image: UploadFile = File(...)):
     image_data=await image.read()
     decoded=cv2.imdecode(np.frombuffer(image_data, np.uint8),-1)
@@ -32,18 +45,38 @@ async def make_bounding_box(image: UploadFile = File(...)):
     face_classifier=cv2.CascadeClassifier(
         cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
     )
+    face_present=False
     face = face_classifier.detectMultiScale(
         gray_scale, scaleFactor=1.1, minNeighbors=5, minSize=(40,40)
     )
+
     for (x,y,w,h) in face:
         cv2.rectangle(decoded,(x,y),(x+w,y+h),(0,255,0),4)
+        face_present=True
     img_rgb = cv2.cvtColor(decoded, cv2.COLOR_BGR2RGB)
     img=Image.fromarray(img_rgb)
+    img.show()
     buffered = BytesIO()
     img.save(buffered, format="JPEG")
     img_str = base64.b64encode(buffered.getvalue()).decode()
 
-    return JSONResponse(content=jsonable_encoder({"status":200,"message":"annotated","result":img_str}))
+    # Save the image to a temporary file
+    with open('temp.jpg', 'wb') as f:
+        f.write(buffered.getvalue())
+
+    # Upload the image to Cloudinary
+    upload_result = cloudinary.uploader.upload('temp.jpg')
+
+    # Delete the temporary file
+    os.remove('temp.jpg')
+
+    result={
+        "face_present":face_present,
+        "data": upload_result['secure_url']  # The URL of the uploaded image
+    }
+    return JSONResponse(content=jsonable_encoder({"status":200,"message":"annotated","result":result}))
+
+
 if __name__=="__main__":
     parser=argparse.ArgumentParser(description="Run FastApi app")
     parser.add_argument("-p","--port",type=int,default=8084)
